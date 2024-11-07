219f66b6bfd7d91b8c48205548fbc8580c6d8e03
Sakurafuld <168341875+Sakurafuld@users.noreply.github.com>
2024-11-07 20:06:17 +0900

    Delete src/main directory

--- src/main/java/com/sakurafuld/oneness/content/oneness/OnenessTouchItem.java
+++
@@ -1,9 +0,0 @@
-package com.sakurafuld.oneness.content.oneness;
-
-import com.sakurafuld.oneness.api.touch.TouchItem;
-
-public class OnenessTouchItem extends TouchItem {
-    public OnenessTouchItem(Properties pProperties) {
-        super(pProperties.tab(null));
-    }
-}
